2022257ac1d220743b267635d6e89ad79d5e7236
Hyde <hydesrealmail@gmail.com>
2025-12-07 20:19:43 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    DefaultTerminal, Frame,
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+};
+
+#[derive(Debug, Default)]
+pub struct FileManager {
+    path: String,
+    exit: bool,
+}
+
+impl FileManager {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        while !self.exit {
+            terminal.draw(|frame| self.draw(frame))?;
+            self.handle_events()?;
+        }
+        Ok(())
+    }
+
+    fn exit(&mut self) {
+        self.exit = true;
+    }
+
+    fn draw(&self, frame: &mut Frame) {
+        frame.render_widget(self, frame.area());
+    }
+
+    fn handle_events(&mut self) -> io::Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                self.handle_key_event(key_event)
+            }
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn handle_key_event(&mut self, key: KeyEvent) {
+        match key.code {
+            KeyCode::Char('q') => self.exit(),
+            _ => {}
+        }
+    }
+}
+
+impl Widget for &FileManager {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Line::from(" Gravily File Manager ".bold());
+        let instructions = Line::from(vec![
+            " Move Left ".into(),
+            "<Left>".blue().bold(),
+            " Move Right ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<q or esc> ".blue().bold(),
+        ]);
+        let block = Block::bordered()
+            .title(title.centered())
+            .title_bottom(instructions.centered())
+            .border_set(border::THICK);
+
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Current Path: ".into(),
+            self.path.to_string().yellow(),
+        ])]);
+
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
+    }
+}
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let result = FileManager::default().run(&mut terminal);
+
+    ratatui::restore();
+
+    result
+}
